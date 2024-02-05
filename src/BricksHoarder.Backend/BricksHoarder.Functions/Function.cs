8c1744ce8c94ece5c1ee5f072068f78f6511290b
Andrzej Gołaszewski <andrusza1989@gmail.com>
2024-02-05 09:29:32 +0100

    test

--- src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
+++ src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
@@ -1,3 +1,4 @@
+using BricksHoarder.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -8,16 +9,19 @@ namespace BricksHoarder.Functions
     public class Function
     {
         private readonly ILogger<Function> _logger;
+        private readonly ICacheService _cacheService;
 
-        public Function(ILogger<Function> logger)
+        public Function(ILogger<Function> logger, ICacheService cacheService)
         {
             _logger = logger;
+            _cacheService = cacheService;
         }
 
         [Function("HttpFunction")]
         public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
         {
+            _cacheService.SetAsync("TEST", "TEST", TimeSpan.FromDays(1));
             return new OkObjectResult($"123Welcome to Azure Functions, {req.Query["name"]}!");
         }
     }
