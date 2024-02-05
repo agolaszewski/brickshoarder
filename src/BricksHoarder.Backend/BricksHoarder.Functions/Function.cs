5af882b9e90efa8c6c6f6aa873688b8f6a0c5f23
Andrzej Gołaszewski <andrusza1989@gmail.com>
2024-02-05 19:11:20 +0100

    test

--- src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
+++ src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
@@ -2,7 +2,9 @@ using BricksHoarder.Core.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Configuration;
 
 namespace BricksHoarder.Functions
 {
@@ -10,17 +12,20 @@ namespace BricksHoarder.Functions
     {
         private readonly ILogger<Function> _logger;
         private readonly ICacheService _cacheService;
+        private readonly IConfiguration _configuration;
 
-        public Function(ILogger<Function> logger, ICacheService cacheService)
+        public Function(ILogger<Function> logger, ICacheService cacheService, IConfiguration configuration)
         {
             _logger = logger;
             _cacheService = cacheService;
+            _configuration = configuration;
         }
 
         [Function("HttpFunction")]
         public IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
         {
+            _logger.LogWarning(_configuration["Redis:ConnectionString"]);
             _cacheService.SetAsync("TEST", "TEST", TimeSpan.FromDays(1));
             return new OkObjectResult($"123Welcome to Azure Functions, {req.Query["name"]}!");
         }
