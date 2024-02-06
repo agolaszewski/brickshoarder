fffe860202fcaf6160750d9454b1fc8de0c84f18
Andrzej Gołaszewski <andrusza1989@gmail.com>
2024-02-07 00:20:58 +0100

    Check

--- src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
+++ src/BricksHoarder.Backend/BricksHoarder.Functions/Function.cs
@@ -1,34 +1,25 @@
-//using BricksHoarder.Core.Services;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Azure.Functions.Worker;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.Logging;
-//using System.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
-//namespace BricksHoarder.Functions
-//{
-//    public class Function
-//    {
-//        private readonly ILogger<Function> _logger;
-//        private readonly ICacheService _cacheService;
-//        private readonly IConfiguration _configuration;
+namespace BricksHoarder.Functions
+{
+    public class Function
+    {
+        private readonly ILogger<Function> _logger;
 
-//        public Function(ILogger<Function> logger, ICacheService cacheService, IConfiguration configuration)
-//        {
-//            _logger = logger;
-//            _cacheService = cacheService;
-//            _configuration = configuration;
-//        }
+        public Function(ILogger<Function> logger)
+        {
+            _logger = logger;
+        }
 
-//        [Function("HttpFunction")]
-//        public IActionResult Run(
-//            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
-//        {
-//            _logger.LogWarning("234");
-//            _logger.LogWarning(_configuration["Redis:ConnectionString"]);
-//            _cacheService.SetAsync("TEST", "TEST", TimeSpan.FromDays(1));
-//            return new OkObjectResult($"Welcome to Azure Functions, {req.Query["name"]}!");
-//        }
-//    }
-//}
+        [Function("Check")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            _logger.LogInformation("7");
+            return new OkObjectResult("7");
+        }
+    }
+}
