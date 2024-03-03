368f9f2cd7f993811afe9833171f3c5d1891bd67
Andrzej Gołaszewski <andrusza1989@gmail.com>
2024-03-03 15:44:26 +0100

    turn on

--- src/BricksHoarder.Backend/BricksHoarder.Functions/AppFunctions/RequeueDeadLetterMessagesFunction.cs
+++ src/BricksHoarder.Backend/BricksHoarder.Functions/AppFunctions/RequeueDeadLetterMessagesFunction.cs
@@ -1,22 +1,22 @@
-﻿//using Azure.Messaging.ServiceBus;
-//using BricksHoarder.Azure.ServiceBus.Services;
-//using Microsoft.Azure.Functions.Worker;
+﻿using Azure.Messaging.ServiceBus;
+using BricksHoarder.Azure.ServiceBus.Services;
+using Microsoft.Azure.Functions.Worker;
 
-//namespace BricksHoarder.Functions.AppFunctions
-//{
-//    public class RequeueDeadLetterMessagesFunction
-//    {
-//        private readonly DeadLetterQueueRescheduler _deadLetterQueueRescheduler;
+namespace BricksHoarder.Functions.AppFunctions
+{
+    public class RequeueDeadLetterMessagesFunction
+    {
+        private readonly DeadLetterQueueRescheduler _deadLetterQueueRescheduler;
 
-//        public RequeueDeadLetterMessagesFunction(DeadLetterQueueRescheduler deadLetterQueueRescheduler)
-//        {
-//            _deadLetterQueueRescheduler = deadLetterQueueRescheduler;
-//        }
+        public RequeueDeadLetterMessagesFunction(DeadLetterQueueRescheduler deadLetterQueueRescheduler)
+        {
+            _deadLetterQueueRescheduler = deadLetterQueueRescheduler;
+        }
 
-//        [Function("RequeueDeadLetterMessagesFunction")]
-//        public async Task RunAsync([ServiceBusTrigger("brickshoarder/fault", "default", Connection = "ServiceBusConnectionString")] ServiceBusReceivedMessage message, CancellationToken cancellationToken)
-//        {
-//            await _deadLetterQueueRescheduler.HandleAsync(message);
-//        }
-//    }
-//}
+        [Function("RequeueDeadLetterMessagesFunction")]
+        public async Task RunAsync([ServiceBusTrigger("brickshoarder/fault", "default", Connection = "ServiceBusConnectionString")] ServiceBusReceivedMessage message, CancellationToken cancellationToken)
+        {
+            await _deadLetterQueueRescheduler.HandleAsync(message);
+        }
+    }
+}
